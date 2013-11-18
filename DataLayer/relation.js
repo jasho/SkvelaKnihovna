fb36fbb5c72fae8d3a8ccfa518e1fe15df186493
jasho <roman.jasek@hotmail.com>
2013-11-18 16:20:48 +0100

    Added data to relations

--- DataLayer/relation.js
+++ DataLayer/relation.js
@@ -34,8 +34,40 @@ new SK.Relation(
 	placeId: 1,
 	name: "Vrácení knihy",
 	// Persona wants at the place
-	services: [{name: "WiFi", reason: "Cos it's fucking awesome", isChecked: false}, {name: "Příjemná obsluha", reason: "Jsou blbi!", isChecked: false}]
+	services: [{name: "knihovník", reason: "Je velmi dobré mít knihovníka za pultem, spoustu lidí to potěší a ocení!", isChecked: false},
+			   {name: "lístek s potvrzením výpůjčky", reason: "Je velmi dobré vydávat a na něm informovat o možnostech prodloužení a dokdy knihu vrátit", isChecked: false},
+			   {name: "propisky", reason: "lidé ocení možnost vzít si propisku s logem Vaší knihovny jež jim bude Vás připomínat", isChecked: false},
+			   {name: "vizitku", reason: "s kontaktními informacemi o Vaší pobočce", isChecked: false},
+			   {name: "Free Wi-Fi", reason: "Poskytuje možnost všem lidem se připojit na internet i pomocí svého notebooku či mobilu a zpříjemní jim pobyt ve Vaší knihovně", isChecked: false}]
 }),
+new SK.Relation(
+{
+ personaId: 1,
+ placeId: 7,
+ name: "Prohlížení dětských knížek",
+ services: [{name: "kategorie dětských knížek", reason: "O to má Váš malý čtenář největší zájem", isChecked: false},
+ services: [{name: "vyhledávání", reason: "Aby si najít svojí oblíbenou knihu kterou hledá.", isChecked: false},]
+ services: [{name: "detail knihy", reason: "Detail knihy umožňuje si zjistit o daném svazku více", isChecked: false},]
+ services: [{name: "obrázek", reason: "Ten je přeci pro nejmenší nejdůležitější!", isChecked: false},]]
+}),
+new SK.Relation(
+{
+ personaId: 3,
+ placeId: 1,
+ name: "Vrácení knihy",
+ services: [{name: "Příjemná obsluha", reason: "Příjemný usměvavý knihovník či knihovnice vždy zlepší den každému čtenáři", isChecked: false},
+ 			{name: "Informace o spozdném", reason: "Aby Vaši čtenáři věděli přesně kolik dluží na spozdném.", isChecked: false}]
+}),
+new SK.Relation(
+{
+ personaId: 3,
+ placeId: 7,
+ name: "Hledání otevírací doby",
+ services: [{name: "Viditelná otevírací doba", reason: "Je velmi důležitá, zvlášť pro vytížené čtenáře Vaší knihovny, kteří se k Vám chtějí zastavit i v pozdějších hodinách.", isChecked: false},
+ 			{name: "Telefon", reason: "Pro možnost se okamžitě spojit se svým knihovníkem!", isChecked: false},
+ 			{name: "E-mail", reason: "Je fajn možnost pro komplikovanější problémy, které nejsou neodkladné!", isChecked: false},
+ 			{name: "přehledné stránky", reason: "přehlednost Vašich stránek by měla být na prvním místě.", isChecked: false}]
+})
 ]
 
 var selectedRelations = [relations[0]];
